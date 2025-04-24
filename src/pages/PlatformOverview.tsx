aba8a64b4d2b8129ca537a4af0342d3909ae6153
MoniratnaKohin <moni@kohin.xyz>
2025-04-24 12:34:55 +0530

    added only combo bets

--- src/pages/PlatformOverview.tsx
+++ src/pages/PlatformOverview.tsx
@@ -1,181 +1,181 @@
 import { useBetData } from "../hooks/useBetData";
- import { usePlatformData } from "../hooks/UsePlatformData";
- import { useTvlData } from "../hooks/useTvlData";
- import { formatOrParseNumber } from "../utils/parseNumber";
- import {
- 	FiDatabase,
- 	FiBarChart,
- 	FiDollarSign,
- 	FiShield,
- 	FiDisc,
- } from "react-icons/fi";
- 
- export const PlatformOverview = () => {
- 	const { data: tableData, isLoading } = usePlatformData();
- 	const { data: betData } = useBetData();
- 	const { data: tvlData } = useTvlData();
- 	console.log({ tvlData });
- 
- 	if (isLoading) {
- 		return (
- 			<div className="space-y-8">
- 				<div className="h-[400px] bg-[#171717] rounded-lg shadow animate-pulse" />
- 			</div>
- 		);
- 	}
- 
- 	const stats = [
- 		{
- 			label: "Platform TVL",
- 			value: Number(tvlData) / 10 ** 6,
- 			displayValue: `${formatOrParseNumber(Number(tvlData) / 10 ** 6)} USDT`,
- 			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Platform Volume",
- 			value: Number(tableData?.data?.platformVol),
- 			displayValue: `${formatOrParseNumber(
- 				Number(tableData?.data?.platformVol)
- 			)} USDT`,
- 			icon: <FiBarChart className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Fees Collected",
- 			value: Number(tableData?.data?.totalFees) / 10 ** 6,
- 			displayValue: `${formatOrParseNumber(
- 				Number(tableData?.data?.totalFees) / 10 ** 6
- 			)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Premiums Collected",
- 			value: Number(tableData?.data?.totalPremium),
- 			displayValue: `${formatOrParseNumber(
- 				Number(tableData?.data?.totalPremium)
- 			)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Active Covers",
- 			value: Number(tableData?.data?.totalActiveCovers),
- 			displayValue: Number(tableData?.data?.totalActiveCovers),
- 			icon: <FiShield className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Liquidity Donated",
- 			value: Number(tableData?.data?.liquidityDonated),
- 			displayValue: `${Number(tableData?.data?.liquidityDonated)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 	];
- 	const singleBetStats = [
- 		{
- 			label: "Total Covers",
- 			value: Number(betData?.data?.allSingleBets),
- 			displayValue: Number(betData?.data?.allSingleBets),
- 			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Manual Covers",
- 			value: Number(betData?.data?.manualSingleBets),
- 			displayValue: Number(betData?.data?.manualSingleBets),
- 			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Premium",
- 			value: Number(betData?.data?.totalSingleBetsPremium),
- 			displayValue: `${formatOrParseNumber(
- 				Number(betData?.data?.totalSingleBetsPremium)
- 			)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Payout",
- 			value: Number(betData?.data?.totalSingleBetsPayout),
- 			displayValue: ` ${formatOrParseNumber(
- 				Number(betData?.data?.totalSingleBetsPayout)
- 			)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Avg Odds",
- 			value: Number(betData?.data?.averageSingleBetOdds),
- 			displayValue: formatOrParseNumber(
- 				Number(betData?.data?.averageSingleBetOdds)
- 			),
- 			icon: <FiDisc className="text-[#82FF1F] text-2xl" />,
- 		},
- 	];
- 	const comboBetStats = [
- 		{
- 			label: "Total Covers",
- 			value: Number(betData?.data?.allComboBets),
- 			displayValue: Number(betData?.data?.allComboBets),
- 			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Manual Covers",
- 			value: Number(betData?.data?.manualComboBets),
- 			displayValue: Number(betData?.data?.manualComboBets),
- 			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Premium",
- 			value: Number(betData?.data?.totalComboBetsPremium),
- 			displayValue: `${formatOrParseNumber(
- 				Number(betData?.data?.totalComboBetsPremium)
- 			)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Total Payout",
- 			value: Number(betData?.data?.totalComboBetsPayout),
- 			displayValue: `${formatOrParseNumber(
- 				Number(betData?.data?.totalComboBetsPayout)
- 			)} USDT`,
- 			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
- 		},
- 		{
- 			label: "Avg Odds",
- 			value: Number(betData?.data?.averageComboBetOdds),
- 			displayValue: formatOrParseNumber(
- 				Number(betData?.data?.averageComboBetOdds)
- 			),
- 			icon: <FiDisc className="text-[#82FF1F] text-2xl" />,
- 		},
- 	];
- 
- 	return (
- 		<div className="overscroll-none">
- 			<h1 className="text-2xl font-bold text-[#82FF1F] mb-8">
- 				Platform Overview
- 			</h1>
- 
- 			<div className="bg-[#171717] rounded-lg shadow">
- 				<div className="px-4 sm:px-6 py-6 w-[1000px] xl:w-[1100px]">
- 					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
- 						{stats.map((stat, index) => (
- 							<div
- 								key={index}
- 								className="relative bg-[#2c2c2c] rounded-lg p-6 text-white flex flex-col items-center justify-center shadow-md hover:shadow-lg transition-shadow duration-200 group"
- 							>
- 								<div className="mb-2">{stat.icon}</div>
- 								<div className="text-lg font-semibold">{stat.label}</div>
- 								<div className="text-2xl font-bold text-[#82FF1F]">
- 									{stat.displayValue}
- 								</div>
- 
- 								{/* Tooltip */}
- 								<div className="absolute bottom-12 bg-black text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
- 									Value: {stat.value.toFixed(2)}
- 								</div>
- 							</div>
- 						))}
- 					</div>
- 				</div>
- 			</div>
- 
- 			<h1 className="text-2xl font-bold text-[#82FF1F] mb-8">
+import { usePlatformData } from "../hooks/UsePlatformData";
+import { useTvlData } from "../hooks/useTvlData";
+import { formatOrParseNumber } from "../utils/parseNumber";
+import {
+	FiDatabase,
+	FiBarChart,
+	FiDollarSign,
+	FiShield,
+	FiDisc,
+} from "react-icons/fi";
+
+export const PlatformOverview = () => {
+	const { data: tableData, isLoading } = usePlatformData();
+	const { data: betData } = useBetData();
+	const { data: tvlData } = useTvlData();
+	console.log({ tvlData });
+
+	if (isLoading) {
+		return (
+			<div className="space-y-8">
+				<div className="h-[400px] bg-[#171717] rounded-lg shadow animate-pulse" />
+			</div>
+		);
+	}
+
+	const stats = [
+		{
+			label: "Platform TVL",
+			value: Number(tvlData) / 10 ** 6,
+			displayValue: `${formatOrParseNumber(Number(tvlData) / 10 ** 6)} USDT`,
+			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Platform Volume",
+			value: Number(tableData?.data?.platformVol),
+			displayValue: `${formatOrParseNumber(
+				Number(tableData?.data?.platformVol)
+			)} USDT`,
+			icon: <FiBarChart className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Fees Collected",
+			value: Number(tableData?.data?.totalFees) / 10 ** 6,
+			displayValue: `${formatOrParseNumber(
+				Number(tableData?.data?.totalFees) / 10 ** 6
+			)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Premiums Collected",
+			value: Number(tableData?.data?.totalPremium),
+			displayValue: `${formatOrParseNumber(
+				Number(tableData?.data?.totalPremium)
+			)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Active Covers",
+			value: Number(tableData?.data?.totalActiveCovers),
+			displayValue: Number(tableData?.data?.totalActiveCovers),
+			icon: <FiShield className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Liquidity Donated",
+			value: Number(tableData?.data?.liquidityDonated),
+			displayValue: `${Number(tableData?.data?.liquidityDonated)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+	];
+	const singleBetStats = [
+		{
+			label: "Total Covers",
+			value: Number(betData?.data?.allSingleBets),
+			displayValue: Number(betData?.data?.allSingleBets),
+			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Manual Covers",
+			value: Number(betData?.data?.manualSingleBets),
+			displayValue: Number(betData?.data?.manualSingleBets),
+			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Premium",
+			value: Number(betData?.data?.totalSingleBetsPremium),
+			displayValue: `${formatOrParseNumber(
+				Number(betData?.data?.totalSingleBetsPremium)
+			)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Payout",
+			value: Number(betData?.data?.totalSingleBetsPayout),
+			displayValue: ` ${formatOrParseNumber(
+				Number(betData?.data?.totalSingleBetsPayout)
+			)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Avg Odds",
+			value: Number(betData?.data?.averageSingleBetOdds),
+			displayValue: formatOrParseNumber(
+				Number(betData?.data?.averageSingleBetOdds)
+			),
+			icon: <FiDisc className="text-[#82FF1F] text-2xl" />,
+		},
+	];
+	const comboBetStats = [
+		{
+			label: "Total Covers",
+			value: Number(betData?.data?.allComboBets),
+			displayValue: Number(betData?.data?.allComboBets),
+			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Manual Covers",
+			value: Number(betData?.data?.manualComboBets),
+			displayValue: Number(betData?.data?.manualComboBets),
+			icon: <FiDatabase className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Premium",
+			value: Number(betData?.data?.totalComboBetsPremium),
+			displayValue: `${formatOrParseNumber(
+				Number(betData?.data?.totalComboBetsPremium)
+			)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Total Payout",
+			value: Number(betData?.data?.totalComboBetsPayout),
+			displayValue: `${formatOrParseNumber(
+				Number(betData?.data?.totalComboBetsPayout)
+			)} USDT`,
+			icon: <FiDollarSign className="text-[#82FF1F] text-2xl" />,
+		},
+		{
+			label: "Avg Odds",
+			value: Number(betData?.data?.averageComboBetOdds),
+			displayValue: formatOrParseNumber(
+				Number(betData?.data?.averageComboBetOdds)
+			),
+			icon: <FiDisc className="text-[#82FF1F] text-2xl" />,
+		},
+	];
+
+	return (
+		<div className="overscroll-none">
+			<h1 className="text-2xl font-bold text-[#82FF1F] mb-8">
+				Platform Overview
+			</h1>
+
+			<div className="bg-[#171717] rounded-lg shadow">
+				<div className="px-4 sm:px-6 py-6 w-[1000px] xl:w-[1100px]">
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+						{stats.map((stat, index) => (
+							<div
+								key={index}
+								className="relative bg-[#2c2c2c] rounded-lg p-6 text-white flex flex-col items-center justify-center shadow-md hover:shadow-lg transition-shadow duration-200 group"
+							>
+								<div className="mb-2">{stat.icon}</div>
+								<div className="text-lg font-semibold">{stat.label}</div>
+								<div className="text-2xl font-bold text-[#82FF1F]">
+									{stat.displayValue}
+								</div>
+
+								{/* Tooltip */}
+								<div className="absolute bottom-12 bg-black text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+									Value: {stat.value.toFixed(2)}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+
+			{/* <h1 className="text-2xl font-bold text-[#82FF1F] mb-8">
  				Single Covers overview
  			</h1>
  
@@ -193,7 +193,7 @@ import { useBetData } from "../hooks/useBetData";
  									{stat.displayValue}
  								</div>
  
- 								{/* Tooltip */}
+ 								
  								<div className="absolute bottom-12 bg-black text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
  									Value: {stat.value.toFixed(2)}
  								</div>
@@ -201,34 +201,34 @@ import { useBetData } from "../hooks/useBetData";
  						))}
  					</div>
  				</div>
- 			</div>
- 			<h1 className="text-2xl font-bold text-[#82FF1F] mb-8">
- 				Combo Covers overview
- 			</h1>
- 
- 			<div className="bg-[#171717] rounded-lg shadow">
- 				<div className="px-4 sm:px-6 py-6 w-[1000px] xl:w-[1100px]">
- 					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
- 						{comboBetStats.map((stat, index) => (
- 							<div
- 								key={index}
- 								className="relative bg-[#2c2c2c] rounded-lg p-6 text-white flex flex-col items-center justify-center shadow-md hover:shadow-lg transition-shadow duration-200 group"
- 							>
- 								<div className="mb-2">{stat.icon}</div>
- 								<div className="text-lg font-semibold">{stat.label}</div>
- 								<div className="text-2xl font-bold text-[#82FF1F]">
- 									{stat.displayValue}
- 								</div>
- 
- 								{/* Tooltip */}
- 								<div className="absolute bottom-12 bg-black text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
- 									Value: {stat.value.toFixed(2)}
- 								</div>
- 							</div>
- 						))}
- 					</div>
- 				</div>
- 			</div>
- 		</div>
- 	);
- };
+ 			</div> */}
+			<h1 className="text-2xl font-bold text-[#82FF1F] mb-8">
+				Combo Covers overview
+			</h1>
+
+			<div className="bg-[#171717] rounded-lg shadow">
+				<div className="px-4 sm:px-6 py-6 w-[1000px] xl:w-[1100px]">
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+						{comboBetStats.map((stat, index) => (
+							<div
+								key={index}
+								className="relative bg-[#2c2c2c] rounded-lg p-6 text-white flex flex-col items-center justify-center shadow-md hover:shadow-lg transition-shadow duration-200 group"
+							>
+								<div className="mb-2">{stat.icon}</div>
+								<div className="text-lg font-semibold">{stat.label}</div>
+								<div className="text-2xl font-bold text-[#82FF1F]">
+									{stat.displayValue}
+								</div>
+
+								{/* Tooltip */}
+								<div className="absolute bottom-12 bg-black text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+									Value: {stat.value.toFixed(2)}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
